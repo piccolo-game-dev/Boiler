a0ca6d73f5d9bd17a5fe1c36ba2ad18821166aa6
piccolo-game-dev <piccolo.game.dev@gmail.com>
2026-07-20 23:12:35 +0900

    ゴミオブジェクトベース作成

---
+++ Assets/Boiler/Scripts/Garbage/GarbageBase.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Garbage
+{
+    public abstract class GarbageBase : MonoBehaviour
+    {
+
+        [SerializeField] private Rigidbody _rigidbody = null;
+
+        public Rigidbody Rigidbody => _rigidbody;
+
+        private void Awake()
+        {
+            if (_rigidbody == null)
+            {
+                _rigidbody = GetComponent<Rigidbody>();
+            }
+
+            if (!tag.Contains(GarbageConstant.GARBAGE_TAG))
+            {
+                tag = GarbageConstant.GARBAGE_TAG;
+            }
+        }
+    }
+}
---
+++ Assets/Boiler/Scripts/Garbage/GarbageConstant.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Garbage
+{
+    public static class GarbageConstant
+    {
+        public const string GARBAGE_TAG = "Garbage";
+    }
+}
